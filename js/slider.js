8247ca7cf3860c0b51cef581fbe376ade9f2285d
Dmitry-login <71512791+Dmitry-login@users.noreply.github.com>
2020-09-24 18:18:34 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function() {
+	 
+      $(".vertical-center").slick({
+      	 dots: true,
+      	 speed: 1000,
+      	 autoplay: true,
+      	 autoplaySpeed: 2000,
+      	 fade: true
+      });
+      
+    });
